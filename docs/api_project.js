88ba31a54babf10745fae3dfd55b529eb465c5ee
Caelin <caelinsutch@gmail.com>
2020-06-09 15:51:12 -0700

    Updated docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-06-09T22:38:07.674Z",
+    "time": "2020-06-09T22:51:01.940Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
--- src/index.ts
+++ src/index.ts
@@ -6,37 +6,51 @@ import {Request, Response} from 'express';
 const app = express();
 const port = process.env.PORT || 2000;
 const mode = process.env.NODE_ENV || 'production';
+const root = '/badges';
 
 /**
  * @api {get} / Get the Home Information
  * @apiName Get Home
  * @apiGroup Home
  */
-app.get('/api', (req: Request, res: Response) => {
+app.get(root, (req: Request, res: Response) => {
   res.send(`Hello World! Welcome to the BLM Badge Repo! See URL for the available endpoints and badges!`)
 })
 
 /**
- * @api {get} /blm Badge that says "SUPPORTS BLM" with two colors
- * @apiName Get BLM Badge
+ * @api {get} /supports-blm Badge that says "SUPPORTS BLM" with two colors
+ * @apiName Supports BLM Badge
  * @apiGroup Badges
  *
  * @apiParam (Query Parameters) {String} color1=#808080 Color of the first half of the badge
  * @apiParam (Query Parameters) {String} color2=#c83737ff Color of the second half of the badge
  */
-app.get('/api/supports-blm', (req: Request, res: Response) => {
+app.get(root + '/supports-blm', (req: Request, res: Response) => {
   res.send(BadgeBuilder.supportsBlm({
     color1: req.query.color1 as string || undefined,
     color2: req.query.color2 as string || undefined
   }))
 })
 
-app.get('/api/blm', (req: Request, res: Response) => {
+/**
+ * @api {get} /blm Badge that says "Black Lives Matter"
+ * @apiName BLM Badge
+ * @apiGroup Badges
+ *
+ * @apiParam (Query Parameters) {String} color=#808080 Color of the first half of the badge
+ */
+app.get(root + '/blm', (req: Request, res: Response) => {
   res.send(BadgeBuilder.blm({
-    color: req.query.color1 as string || undefined
+    color: req.query.color as string || undefined
   }))
 })
-app.get('/api/we-love-blm', (req: Request, res: Response) => {
+
+/**
+ * @api {get} /blm Badge that says We Heart BLM
+ * @apiName We Heart BLM
+ * @apiGroup Badges
+ */
+app.get(root + '/we-love-blm', (req: Request, res: Response) => {
   res.send(BadgeBuilder.weLoveBlm())
 })
 
